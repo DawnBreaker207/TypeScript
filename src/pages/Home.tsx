205f832feabe3640350e1d84784129b2b435a98d
DawnBreaker <tunganhngo207@gmail.com>
2024-03-19 11:13:20 +0700

    Add: add router

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
 import Footer from '~/components/Footer'
 import Header from '~/components/Header'
 import Product from '~/components/Product'
 import ProductList from '~/components/ProductList'
+import NotFound from './NotFound'
 
 const Home = () => {
   return (
     <>
       <Header />
-      <ProductList />
+      <Routes>
+        <Route path='/' element={<ProductList />} />
+        <Route path='/products' element={<ProductList />} />
+        {/* <Route path='/products/detail' element={<Product />} /> */}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   )
