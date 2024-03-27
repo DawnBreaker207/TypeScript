90eb747ff4cf725fcf1fa9e347b7ef7818730fee
DawnBreaker <tunganhngo207@gmail.com>
2024-03-27 15:55:09 +0700

    Update: Refactor code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from './pages/Home'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from './pages/NotFound'
-import Products from './pages/Products'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import ProductDetail from './pages/ProductDetail'
-import Banner from './components/Banner/Banner-Boostrap'
+import Dashboard from './pages/admin/Dashboard'
+import { Product } from './interfaces/Product'
+import { getAllProducts } from './apis/product'
+// import Banner from './components/Banner/Banner-Boostrap'
 
 const App = () => {
+  const [products, setProducts] = useState<Product[]>([])
+  useEffect(() => {
+    ;(async () => {
+      const data = await getAllProducts()
+      setProducts(data)
+    })()
+  }, [])
+
   return (
     <>
       <Header />
       {/* <Banner /> */}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<Products />} />
+        <Route index element={<Home product={products} />} />
+        <Route path='/shop' element={<Home product={products} />} />
         <Route path='/shop/:id' element={<ProductDetail />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/admin' element={<Dashboard />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
---
+++ src/pages/admin/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FormAdmin from '~/components/Form/FormAdmin'
+
+const Dashboard = () => {
+  return (
+    <>
+      <FormAdmin />
+      {/* <h1>Test</h1> */}
+    </>
+  )
+}
+
+export default Dashboard
